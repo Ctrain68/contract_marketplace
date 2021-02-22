532ea24ad4a49529940240671fb0563ac5128b75
Colin Forster <colinjforster@gmail.com>
2021-02-22 16:42:58 +1000

    flask upgrade check

--- script1.sh
+++ script1.sh
@@ -3,4 +3,4 @@
 aws s3 cp s3://col-env-bucket/contract_app_env/.env /home/github-actions/contract_marketplace/.env
 
 
-gunicorn -b 0.0.0.0 -w 3 "src:create_app()"
+# gunicorn -b 0.0.0.0 -w 3 "src:create_app()"
