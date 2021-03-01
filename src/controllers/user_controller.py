91960d1e832682f7070b6f97dd1b1f0873f284a6
Colin Forster <colinjforster@gmail.com>
2021-03-01 14:03:18 +1000

    creating auth form

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -5,6 +5,7 @@ from src.models.User import User
 from src import bcrypt, db
 from flask_jwt_extended import create_access_token
 from datetime import timedelta
+from src.forms.UserForm import RegistrationForm, LoginForm
 
 auth = Blueprint('auth', __name__, url_prefix="/auth")
 
@@ -16,13 +17,14 @@ def auth_register():
 
     if user:
         return abort(400, description="User already")
-    
-    user = User()
-    user.email = user_fields["email"]
-    user.password = bcrypt.generate_password_hash(user_fields["password"]).decode("utf-8")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User()
+        user.email = form.email.data
+        user.password = bcrypt.generate_password_hash(form.data.password).decode("utf-8")
 
-    db.session.add(user)
-    db.session.commit()
+        db.session.add(user)
+        db.session.commit()
 
     return jsonify(user_schema.dump(user))
 
@@ -30,8 +32,10 @@ def auth_register():
 @auth.route("/login", methods=["POST"])
 def auth_login():
     user_fields = user_schema.load(request.json)
+    form = LoginForm()
+    if form.validate_on_submit
 
-    user = User.query.filter_by(email=user_fields["email"]).first()
+        user = User.query.filter_by(email=user_fields["email"]).first()
 
     if not user or not bcrypt.check_password_hash(user.password, user_fields["password"]):
         return abort(401, description="Incorrect username or password")
